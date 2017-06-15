23af66c383d626a2c4bfe9d2b6de3e7b357daf03
Tayo Madein <tuby1805@gmail.com>
2017-06-15 23:02:52 +0100

    Game history can now be stored and revisited

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="status">{status}</div>
+        <div className="status"></div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -46,12 +46,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -62,14 +63,22 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ? false : true,
+    });
+  }
+
   render() {
-    const winner = calculateWinner(this.state.squares);
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
     
     const moves = history.map((step, move) => {
       const desc = move ?
